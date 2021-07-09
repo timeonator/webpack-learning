fd00df859338cf99fe4fd23aac35c3e1897b8805
timeonator <timeonator@gmail.com>
2021-07-08 21:52:31 -0700

    added some async code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 const App = () => {
+    const fetchDataPackage = async () => {
+        let response = await fetch("http://localhost:8000/datapackages",{
+            method: 'GET',
+            mode: 'cors',
+            cache: 'default',
+        });
+        let data = await response.json()
+        return(data)
+    }
+    const mydata = fetchDataPackage()
     return(<h1> React Webpack, Yeah Hah!! </h1>)
 }
 
